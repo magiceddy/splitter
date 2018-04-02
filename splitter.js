44ffad5ae29f183e546cb9cf497972ffa97f068f
magiceddy <rinaldo.rossi.web@gmail.com>
2018-04-02 20:36:26 +0200

    inject web3 provider

---
+++ splitter.js
@@ -0,0 +1,9 @@
+const Web3 = require('web3');
+
+if(typeof web3 !== 'undefined') {
+	web3 = new Web3(web3.currentProvider);
+} else {
+	web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+}
+
+console.log(web3.version.network);
