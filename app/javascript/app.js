d0a110f343d54dd93c3f6a09d67af7965e61794a
magiceddy <rinaldo.rossi.web@gmail.com>
2018-04-03 22:25:11 +0200

    Display and update balance

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -15,9 +15,10 @@ const splitter = contract(Splitter);
 window.App = {
     start: async function() {
 
-        this.startConfig();
         splitter.setProvider(web3.currentProvider);
-        instance = await splitter.deployed();
+        this.instance = await splitter.deployed();
+        this.startConfig();
+        this.registerEvents();
     },
     send: async function() {
         const sender = document.getElementById('coinbase').value;
@@ -26,16 +27,15 @@ window.App = {
         const amount = web3.toBigNumber(document.getElementById('amount').value);
         const amountInWei = web3.toWei(amount, 'ether');
 
-        let txObject = await instance.split(ben1, ben2, { from: sender, value: amountInWei });
+        const txObject = await this.instance.split(ben1, ben2, { from: sender, value: amountInWei });
     },
     startConfig: function() {
         this.coinbase = web3.eth.coinbase;
 
         document.getElementById('coinbase').value = this.coinbase;
-        document.getElementById('amount').value = 'Ether';
 
         return web3.version.network == 3 ? 
-            self.configForRopsten() : 
+            this.configForRopsten() : 
             this.configForTestRpc();
     },
     configForRopsten: function() {},
@@ -45,6 +45,34 @@ window.App = {
         document.getElementById('firstBeneficiary').value = this.accounts[1];
         document.getElementById('secondBeneficiary').value =this.accounts[2];
         document.getElementById('network').innerText = "Network: Local TestRpc";
+
+        this.setBalance();
+    },
+    onAddressChange: function(event) {
+        console.log(event);
+    },
+    setBalance: async function () {
+        const senderBalanceInWei = await web3.eth.getBalance(this.coinbase);
+        const firstBalanceInWei = await web3.eth.getBalance(this.accounts[1]);
+        const secondBalanceInWei = await web3.eth.getBalance(this.accounts[2]);
+        const senderBalance = web3.fromWei(senderBalanceInWei, 'ether').toString(10);
+        const firstBalance = web3.fromWei(firstBalanceInWei, 'ether').toString(10);
+        const secondBalance = web3.fromWei(secondBalanceInWei, 'ether').toString(10);
+
+        document.getElementById('senderBalance').innerText = senderBalance + " ETH";
+        document.getElementById('firstBalance').innerText = firstBalance + " ETH";
+        document.getElementById('secondBalance').innerText = secondBalance + " ETH";
+        
+    },
+    registerEvents: function() {
+        const self = this;
+        var event = self.instance.LogSplit({ fromBlock: 0, toBlock: 'latest'});
+
+        event.watch(function(err, res) {
+            if (!err) {
+                self.setBalance();
+            }
+        });
     }
 };
 
