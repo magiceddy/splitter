fddcb769003f44f965418466f9c892cb27b8eb00
magiceddy <rinaldo.rossi.web@gmail.com>
2018-04-03 17:07:01 +0200

    basic implementation of send coin

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -2,27 +2,36 @@ const Splitter = require('../../build/contracts/Splitter.json');
 const contract = require('truffle-contract');
 const Web3 = require('web3');
 
-if(typeof web3 !== 'undefined') {
-	web3 = new Web3(web3.currentProvider);
-} else {
-	web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-}
+let accounts;
+let currAddress;
+let instance;
 
-console.log('ciaoooo');
-
-const accounts = web3.eth.accounts;
-const currAddress = web3.eth.coinbase;
 const splitter = contract(Splitter); 
 
 window.App = {
-    start: function() {
+    start: async function() {
         const self = this;
+        accounts = web3.eth.accounts;
+        coinbase = web3.eth.coinbase;
 
+        console.log(web3.currentProvider);
         splitter.setProvider(web3.currentProvider);
+        instance = await splitter.deployed();
     },
-    send: function() {
-        console.log('ciaooooo');
+    send: async function() {
+        let txObject = await instance.split(accounts[1], accounts[2], { from: coinbase, value: 10 });
+        console.log(txObject);
     }
 };
+
+window.addEventListener('load', function() {
+    if(typeof web3 !== 'undefined') {
+        web3 = new Web3(web3.currentProvider);
+    } else {
+        web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    }
+
+    App.start();
+})
 
 
